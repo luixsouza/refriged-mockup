ded621ee31ecc985bbbb89e046044b7129f1203f
Luis Eduardo <luixst1@gmail.com>
2025-07-02 14:32:34 -0300

    Estrutura inicial do log generator

---
+++ log-generator-mockup/src/main/java/com/refriged/log_generator_mockup/LogGeneratorMockupApplication.java
@@ -0,0 +1,13 @@
+package com.refriged.log_generator_mockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogGeneratorMockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogGeneratorMockupApplication.class, args);
+	}
+
+}
---
+++ log-generator-mockup/src/test/java/com/refriged/log_generator_mockup/LogGeneratorMockupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refriged.log_generator_mockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogGeneratorMockupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
